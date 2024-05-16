63e8a0bd60f1b9db5d2a7054fd75755f0c9f7cd0
Lamont Crook <lamont@adobe.com>
2024-05-16 16:16:45 -0600

    update

---
+++ blocks/hero/hero.js
@@ -0,0 +1,15 @@
+export default function decorate(block) {
+  const title = block.querySelector('.hero > div:nth-child(2)');
+  title.classList.add('hero-title');
+
+  const description = block.querySelector('.hero > div:nth-child(3)');
+  description.classList.add('hero-description');
+
+  const content = document.createElement('div');
+  content.classList.add('hero-content');
+  content.appendChild(title);
+  content.appendChild(description);
+
+  const div = block.querySelector('div');
+  div.append(content);
+}
