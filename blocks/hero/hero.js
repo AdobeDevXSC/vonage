e4116102f61ce67ed3a088641a9387ea6d631ee5
Nikhil Lingannagari <nikhil.lingannagari@vonage.com>
2024-06-28 11:29:10 -0500

    Update styles for text

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -7,11 +7,15 @@ export default function decorate(block) {
   const description = block.querySelector('.hero > div:nth-child(3)');
   description.classList.add('hero-description');
 
+  const nikhiltest = block.querySelector('.hero > div:nth-child(3)');
+  nikhiltest.classList.add('nikhil-description');
+
   const content = document.createElement('div');
   content.classList.add('hero-content');
   content.appendChild(title);
   content.appendChild(description);
   content.appendChild(button);
+  content.appendChild(nikhiltest);
 
   nextSib.remove();
 
